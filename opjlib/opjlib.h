f949c4326f04d9e4315456bb674ef51e096a416a
richstoner <stonerri@gmail.com>
2013-11-26 18:17:17 -0800

    Initial Commit

---
+++ opjlib/opjlib.h
@@ -0,0 +1,13 @@
+//
+//  opjlib.h
+//  opjlib
+//
+//  Created by Rich Stoner on 11/26/13.
+//  Copyright (c) 2013 WholeSlide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface opjlib : NSObject
+
+@end
---
+++ opjlib/opjlib.m
@@ -0,0 +1,13 @@
+//
+//  opjlib.m
+//  opjlib
+//
+//  Created by Rich Stoner on 11/26/13.
+//  Copyright (c) 2013 WholeSlide. All rights reserved.
+//
+
+#import "opjlib.h"
+
+@implementation opjlib
+
+@end
